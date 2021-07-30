2ac194e7b0718c7dbc76e8853d627359737ca9da
Daniele <daniele@horizenlabs.io>
2021-07-30 10:17:21 +0200

    Updated constant value

--- src/type_mapping.rs
+++ src/type_mapping.rs
@@ -2,4 +2,4 @@ pub use cctp_primitives::type_mapping::*;
 
 pub const CUSTOM_DATA_MAX_SIZE: usize = 1024; //in bytes
 pub const UINT_256_SIZE: usize = 32; //in bytes
-pub const UINT_160_SIZE: usize = MC_PK_SIZE; //in bytes
+pub const UINT_160_SIZE: usize = 20; //in bytes
