d30f8a442025c9a3752eb8cf3dbef30e00f92949
Daniele Rogora <danielerogora@horizenlabs.io>
2022-10-07 15:28:55 +0200

    Add sampleCalls tests for key rotation support
    This also updates the reference to the zendoo-cctp-lib repo.

--- examples/sampleCalls.cpp
+++ examples/sampleCalls.cpp
@@ -607,7 +607,8 @@ TEST_SUITE("Single Proof Verifier") {
         std::string proof_path,
         std::string pk_path,
         std::string vk_path,
-        bool constant_present
+        bool constant_present,
+        bool prev_hash_present = false
     ) {
         CctpErrorCode ret_code = CctpErrorCode::OK;
 
@@ -622,6 +623,8 @@ TEST_SUITE("Single Proof Verifier") {
         uint64_t quality = 100;
         uint64_t btr_fee = 1000;
         uint64_t ft_min_amount = 5000;
+
+        field_t* prev_hash = prev_hash_present ? zendoo_get_field_from_long(7) : nullptr;
 
         //Create dummy bt list
         size_t bt_list_len = numBt;
@@ -659,7 +662,7 @@ TEST_SUITE("Single Proof Verifier") {
             zendoo_create_cert_test_proof(
                 zk, constant, sc_id, epoch_number, quality, bt_list_ptr, bt_list_len,
                 custom_fields, 2, end_cum_comm_tree_root, btr_fee, ft_min_amount,
-                sc_pk, (path_char_t*)proof_path.c_str(), proof_path.size(), NUM_CONSTRAINTS, NULL, &ret_code
+                sc_pk, (path_char_t*)proof_path.c_str(), proof_path.size(), NUM_CONSTRAINTS, prev_hash, &ret_code
             ) == true
         );
         CHECK(ret_code == CctpErrorCode::OK);
@@ -706,7 +709,7 @@ TEST_SUITE("Single Proof Verifier") {
             zendoo_verify_certificate_proof(
                 constant, sc_id, epoch_number, quality, bt_list_ptr, bt_list_len,
                 custom_fields, 2, end_cum_comm_tree_root,
-                btr_fee, ft_min_amount, sc_proof, sc_vk, NULL, &ret_code
+                btr_fee, ft_min_amount, sc_proof, sc_vk, prev_hash, &ret_code
             ) == true
         );
         CHECK(ret_code == CctpErrorCode::OK);
@@ -717,7 +720,7 @@ TEST_SUITE("Single Proof Verifier") {
             zendoo_verify_certificate_proof(
                 constant, wrong_sc_id, epoch_number, quality, bt_list_ptr, bt_list_len,
                 custom_fields, 2, end_cum_comm_tree_root, btr_fee, ft_min_amount,
-                sc_proof, sc_vk, NULL, &ret_code
+                sc_proof, sc_vk, prev_hash, &ret_code
             ) == false
         );
         CHECK(ret_code == CctpErrorCode::OK);
@@ -767,6 +770,38 @@ TEST_SUITE("Single Proof Verifier") {
         remove(vk_path.c_str());
     }
 
+    TEST_CASE("Proof Verifier: Cert - Coboundary Marlin - Keyrot") {
+        CctpErrorCode ret_code = CctpErrorCode::OK;
+
+        // Generate cert test circuit pk and vk
+        CHECK(
+            zendoo_generate_mc_test_params(
+                TestCircuitType::Certificate,
+                ProvingSystem::CoboundaryMarlin,
+                NUM_CONSTRAINTS,
+                true,
+                (path_char_t*)params_dir.c_str(),
+                params_dir_len,
+                &ret_code
+            ) == true
+        );
+        CHECK(ret_code == CctpErrorCode::OK);
+
+        auto proof_path = params_dir + std::string("/cob_marlin_cert_test_proof");
+        auto pk_path = params_dir + std::string("/cob_marlin_cert_test_pk");
+        auto vk_path = params_dir + std::string("/cob_marlin_cert_test_vk");
+
+        // Test all cases
+        create_verify_cert_proof(10, true, proof_path, pk_path, vk_path, true, true);
+        create_verify_cert_proof(0, true, proof_path, pk_path, vk_path, true, true);
+        create_verify_cert_proof(10, false, proof_path, pk_path, vk_path, true, true);
+        create_verify_cert_proof(0, false, proof_path, pk_path, vk_path, true, true);
+
+        // Delete files
+        remove(pk_path.c_str());
+        remove(vk_path.c_str());
+    }
+
     TEST_CASE("Proof Verifier: CertNoConst - Coboundary Marlin") {
         CctpErrorCode ret_code = CctpErrorCode::OK;
 
@@ -793,6 +828,38 @@ TEST_SUITE("Single Proof Verifier") {
         create_verify_cert_proof(0, true, proof_path, pk_path, vk_path, false);
         create_verify_cert_proof(10, false, proof_path, pk_path, vk_path, false);
         create_verify_cert_proof(0, false, proof_path, pk_path, vk_path, false);
+
+        // Delete files
+        remove(pk_path.c_str());
+        remove(vk_path.c_str());
+    }
+
+    TEST_CASE("Proof Verifier: CertNoConst - Coboundary Marlin - Keyrot") {
+        CctpErrorCode ret_code = CctpErrorCode::OK;
+
+        // Generate cert test circuit pk and vk
+        CHECK(
+            zendoo_generate_mc_test_params(
+                TestCircuitType::CertificateNoConstant,
+                ProvingSystem::CoboundaryMarlin,
+                NUM_CONSTRAINTS,
+                true,
+                (path_char_t*)params_dir.c_str(),
+                params_dir_len,
+                &ret_code
+            ) == true
+        );
+        CHECK(ret_code == CctpErrorCode::OK);
+
+        auto proof_path = params_dir + std::string("/cob_marlin_cert_no_const_test_proof");
+        auto pk_path = params_dir + std::string("/cob_marlin_cert_no_const_test_pk");
+        auto vk_path = params_dir + std::string("/cob_marlin_cert_no_const_test_vk");
+
+        // Test all cases
+        create_verify_cert_proof(10, true, proof_path, pk_path, vk_path, false, true);
+        create_verify_cert_proof(0, true, proof_path, pk_path, vk_path, false, true);
+        create_verify_cert_proof(10, false, proof_path, pk_path, vk_path, false, true);
+        create_verify_cert_proof(0, false, proof_path, pk_path, vk_path, false, true);
 
         // Delete files
         remove(pk_path.c_str());
@@ -831,6 +898,38 @@ TEST_SUITE("Single Proof Verifier") {
        remove(vk_path.c_str());
     }
 
+    TEST_CASE("Proof Verifier: Cert - Darlin - Keyrot") {
+       CctpErrorCode ret_code = CctpErrorCode::OK;
+
+       // Generate cert test circuit pk and vk
+       CHECK(
+           zendoo_generate_mc_test_params(
+               TestCircuitType::Certificate,
+               ProvingSystem::Darlin,
+               NUM_CONSTRAINTS,
+               true,
+               (path_char_t*)params_dir.c_str(),
+               params_dir_len,
+               &ret_code
+           ) == true
+       );
+       CHECK(ret_code == CctpErrorCode::OK);
+
+        auto proof_path = params_dir + std::string("/darlin_cert_test_proof");
+        auto pk_path = params_dir + std::string("/darlin_cert_test_pk");
+        auto vk_path = params_dir + std::string("/darlin_cert_test_vk");
+
+        // Test all cases
+        create_verify_cert_proof(10, true, proof_path, pk_path, vk_path, true, true);
+        create_verify_cert_proof(0, true, proof_path, pk_path, vk_path, true, true);
+        create_verify_cert_proof(10, false, proof_path, pk_path, vk_path, true, true);
+        create_verify_cert_proof(0, false, proof_path, pk_path, vk_path, true, true);
+
+       // Delete files
+       remove(pk_path.c_str());
+       remove(vk_path.c_str());
+    }
+
     TEST_CASE("Proof Verifier: CertNoConst - Darlin") {
        CctpErrorCode ret_code = CctpErrorCode::OK;
 
@@ -857,6 +956,38 @@ TEST_SUITE("Single Proof Verifier") {
         create_verify_cert_proof(0, true, proof_path, pk_path, vk_path, false);
         create_verify_cert_proof(10, false, proof_path, pk_path, vk_path, false);
         create_verify_cert_proof(0, false, proof_path, pk_path, vk_path, false);
+
+       // Delete files
+       remove(pk_path.c_str());
+       remove(vk_path.c_str());
+    }
+
+    TEST_CASE("Proof Verifier: CertNoConst - Darlin - Keyrot") {
+       CctpErrorCode ret_code = CctpErrorCode::OK;
+
+       // Generate cert test circuit pk and vk
+       CHECK(
+           zendoo_generate_mc_test_params(
+               TestCircuitType::CertificateNoConstant,
+               ProvingSystem::Darlin,
+               NUM_CONSTRAINTS,
+               true,
+               (path_char_t*)params_dir.c_str(),
+               params_dir_len,
+               &ret_code
+           ) == true
+       );
+       CHECK(ret_code == CctpErrorCode::OK);
+
+        auto proof_path = params_dir + std::string("/darlin_cert_no_const_test_proof");
+        auto pk_path = params_dir + std::string("/darlin_cert_no_const_test_pk");
+        auto vk_path = params_dir + std::string("/darlin_cert_no_const_test_vk");
+
+        // Test all cases
+        create_verify_cert_proof(10, true, proof_path, pk_path, vk_path, false, true);
+        create_verify_cert_proof(0, true, proof_path, pk_path, vk_path, false, true);
+        create_verify_cert_proof(10, false, proof_path, pk_path, vk_path, false, true);
+        create_verify_cert_proof(0, false, proof_path, pk_path, vk_path, false, true);
 
        // Delete files
        remove(pk_path.c_str());
@@ -1260,7 +1391,7 @@ TEST_SUITE("ZendooBatchProofVerifier") {
                 false, constant, sc_id, epoch_number, quality, NULL, 0,
                 NULL, 0, end_cum_comm_tree_root, btr_fee, ft_min_amount, sc_pk,
                 (path_char_t*)proof_path.c_str(), proof_path.size(), NUM_CONSTRAINTS,
-                NULL, &ret_code, true//, &SUPPORTED_SEGMENT_SIZE
+                NULL, &ret_code, true, &SUPPORTED_SEGMENT_SIZE
             ) == true
         );
         CHECK(ret_code == CctpErrorCode::OK);
